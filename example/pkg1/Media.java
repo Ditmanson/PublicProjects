cd86f4c1cae2b1b53021318f424f4a749875c14f
Ditmanson <97914433+Ditmanson@users.noreply.github.com>
2022-08-25 22:10:08 -0600

    Polymorphic array lists

---
+++ example/pkg1/Media.java
@@ -0,0 +1,25 @@
+
+
+
+package example.pkg1;
+
+/**
+ *
+ * @author travi
+ */
+public class Media
+{
+    protected String title;
+    public Media(String title){
+        this.title=title;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public void setTitle(String newTitle){
+        title=newTitle;
+    }
+    
+}
